a38a271d2d7211749bf6387b32664c4789e53142
Mitsuru Hashimoto <exhikkii@gmail.com>
2018-05-08 20:16:50 +0900

    initial commit

---
+++ .eslintrc.js
@@ -0,0 +1,37 @@
+// module.exports = {
+//     "env": {
+//         "es6": true,
+//         "node": true
+//     },
+//     "plugins": ["node"],
+//     "extends": ["eslint:recommended", "plugin:node/recommended"],
+//     "parserOptions": {
+//         "sourceType": "module",
+//         "ecmaVersion": 2017
+//     },
+//     "rules": {
+//         "indent": [
+//             "error",
+//             2
+//         ],
+//         "linebreak-style": [
+//             "error",
+//             "unix"
+//         ],
+//         "quotes": [
+//             "error",
+//             "single"
+//         ],
+//         "semi": [
+//             "error",
+//             "always"
+//         ],
+//         "node/no-unsupported-features": [
+//             "error", { "version": 7.6 }
+//         ],
+//     }
+// };
+
+module.exports = {
+    "extends": "airbnb-base"
+};
